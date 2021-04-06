248d7c4c3488ff1ce804691582650e980ec4131f
lmonsalve22 <lmonsalve22@gmail.com>
2021-04-06 18:55:11 -0400

    commit

---
+++ Login/Login/Controllers/PlotlyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class PlotlyController : Controller
+    {
+        // GET: Plotly
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Plot_Scatter_Iris()
+        {
+            return View();
+        }
+
+    }
+}
